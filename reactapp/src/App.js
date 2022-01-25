491fa153d4d4ba8a972e809028487154505201a6
samiran2076 <deysamiran44@gmail.com>
2022-01-25 08:47:12 +0000

    names fixed

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import styled from "styled-components";
-import { AccountBox } from "./components/accountBox"; https://github.com/iamneo-production/9bc4bd33-6944-4e06-bd8d-986e08243090.git
+import { AccountBox } from "./components/accountBox"; 
 
 const AppContainer = styled.div`
   width: 100%;
