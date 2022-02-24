de3c3cc363ee019c14eacf7d62315ee4d5e083c9
Shreya Singh <72337463+Shreya479@users.noreply.github.com>
2022-02-24 11:55:42 +0530

    Delete validateInfo.jsx

--- reactapp/src/User/user-forgetPassword/validateInfo.jsx
+++
@@ -1,9 +0,0 @@
-export default function validateInfo(values) {
-    let errors = {};
-    if (!values.username) {
-      errors.email = 'Email required';
-    } else if (!/\S+@\S+\.\S+/.test(values.email)) {
-      errors.email = 'Email address is invalid';
-    }
-    return errors;
-  }
