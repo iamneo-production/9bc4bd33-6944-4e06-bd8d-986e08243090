2dd8c220f4ee678a372b647a8c78bf3163a68e32
Shreya Singh <72337463+Shreya479@users.noreply.github.com>
2022-03-14 19:38:21 +0530

    Create dummyData.js

---
+++ reactapp/src/dummyData.js
@@ -0,0 +1,323 @@
+export const userData = [
+    {
+      name: "January",
+      "Active User": 4000,
+    },
+    {
+      name: "February",
+      "Active User": 3320,
+    },
+    {
+      name: "March",
+      "Active User": 5320,
+    },
+    {
+      name: "April",
+      "Active User": 4000,
+    },
+    {
+      name: "May",
+      "Active User": 6489,
+    },
+    {
+      name: "June",
+      "Active User": 8764,
+    },
+    {
+      name: "July",
+      "Active User": 4364,
+    },
+    {
+      name: "August",
+      "Active User": 3543,
+    },
+    {
+      name: "September",
+      "Active User": 4785,
+    },
+    {
+      name: "October",
+      "Active User": 1000,
+    },
+    {
+      name: "November",
+      "Active User": 4870,
+    },
+    {
+      name: "December",
+      "Active User": 3560,
+    },
+  ];
+
+  export const productData = [
+    {
+      name: "January",
+      "Sales": 4341,
+    },
+    {
+      name: "Feburary",
+      "Sales": 3342,
+    },
+    {
+      name: "March",
+      "Sales": 5324,
+    },
+  ];
+
+  export const userRows = [
+    {
+      id: 1,
+      username: "Monica E. Geller",
+      avatar:
+        "https://cmkt-image-prd.global.ssl.fastly.net/0.1.0/ps/1382917/1160/772/m1/fpnw/wm0/businessman-avatar-icon-01-.jpg?1466426985&s=e682e5d7c8b4cd43735b642490f43f06",
+      email: "mon@gmail.com",
+      status: "active",
+      transaction: "$13420.00",
+    },
+    {
+      id: 2,
+      username: "Rachel Karen Greene",
+      avatar:
+        "https://th.bing.com/th/id/OIP.5JLM9njB9uJr5n1_p0nKFAHaHa?pid=ImgDet&rs=1",
+      email: "rkg@gmail.com",
+      status: "active",
+      transaction: "$13420.00",
+    },
+    {
+      id: 3,
+      username: "Phoebe Buffay-Hannigan",
+      avatar:
+        "https://cmkt-image-prd.global.ssl.fastly.net/0.1.0/ps/1382917/1160/772/m1/fpnw/wm0/businessman-avatar-icon-01-.jpg?1466426985&s=e682e5d7c8b4cd43735b642490f43f06",
+      email: "pbh@gmail.com",
+      status: "active",
+      transaction: "$13420.00",
+    },
+    {
+      id: 4,
+      username: "Chandler Muriel Bing",
+      avatar:
+        "https://th.bing.com/th/id/OIP.5JLM9njB9uJr5n1_p0nKFAHaHa?pid=ImgDet&rs=1",
+      email: "cmb@gmail.com",
+      status: "active",
+      transaction: "$12045.00",
+    },
+    {
+      id: 5,
+      username: "Janice Litman Goralnik ",
+      avatar:
+        "https://cmkt-image-prd.global.ssl.fastly.net/0.1.0/ps/1382917/1160/772/m1/fpnw/wm0/businessman-avatar-icon-01-.jpg?1466426985&s=e682e5d7c8b4cd43735b642490f43f06",
+      email: "jlg@gmail.com",
+      status: "active",
+      transaction: "$12230.00",
+    },
+    {
+      id: 6,
+      username: "Alice Knight",
+      avatar:
+        "https://th.bing.com/th/id/OIP.5JLM9njB9uJr5n1_p0nKFAHaHa?pid=ImgDet&rs=1",
+      email: "ak@gmail.com",
+      status: "active",
+      transaction: "$12230.00",
+    },
+    {
+      id: 7,
+      username: "Elizabeth Stevens",
+      avatar:
+        "https://cmkt-image-prd.global.ssl.fastly.net/0.1.0/ps/1382917/1160/772/m1/fpnw/wm0/businessman-avatar-icon-01-.jpg?1466426985&s=e682e5d7c8b4cd43735b642490f43f06",
+      email: "est@gmail.com",
+      status: "active",
+      transaction: "$12330.00",
+    },
+    {
+      id: 8,
+      username: "Amy Greene",
+      avatar:
+        "https://th.bing.com/th/id/OIP.5JLM9njB9uJr5n1_p0nKFAHaHa?pid=ImgDet&rs=1",
+      email: "amjyg@gmail.com",
+      status: "active",
+      transaction: "$13220.00",
+    },
+    {
+      id: 9,
+      username: "Amanda Buffamonteezi ",
+      avatar:
+        "https://cmkt-image-prd.global.ssl.fastly.net/0.1.0/ps/1382917/1160/772/m1/fpnw/wm0/businessman-avatar-icon-01-.jpg?1466426985&s=e682e5d7c8b4cd43735b642490f43f06",
+      email: "ambffn@gmail.com",
+      status: "active",
+      transaction: "$123202.00",
+    },
+    {
+      id: 10,
+      username: "Jill Goodacre Connick",
+      avatar:
+        "https://th.bing.com/th/id/OIP.5JLM9njB9uJr5n1_p0nKFAHaHa?pid=ImgDet&rs=1",
+      email: "jgcn@gmail.com",
+      status: "active",
+      transaction: "$12043.00",
+    },
+  ];
+
+  export const productRows = [
+    {
+      id: 1,
+      name: "Adolfo’s catfish",
+      img:
+        "https://th.bing.com/th/id/OIP.ijc1_eSTILfmCUKQr7UiGAHaGC?w=247&h=201&c=7&r=0&o=5&dpr=1.38&pid=1.7",
+      stock: 12213,
+      status: "active",
+      price: "$120.00",
+    },
+    {
+      id: 2,
+      name: "Algae eaters",
+      img:
+        "https://th.bing.com/th/id/OIP.HwxpVvWuPwWLOMcSAuT4OgHaE8?pid=ImgDet&rs=1",
+      stock: 12323,
+      status: "active",
+      price: "$125.00",
+    },
+    {
+      id: 3,
+      name: "Altum angelfish",
+      img:
+        "https://th.bing.com/th/id/OIP.cTMvvtbVDDM1yrxszxBaaAHaEI?w=314&h=180&c=7&r=0&o=5&dpr=1.38&pid=1.7",
+      stock: 11123,
+      status: "active",
+      price: "$140.00",
+    },
+    {
+      id: 4,
+      name: "Angelfish",
+      img:
+        "https://th.bing.com/th/id/OIP.d_t-ApyXCjMfBXKcG0kc9QHaFp?w=257&h=196&c=7&r=0&o=5&dpr=1.38&pid=1.7",
+      stock: 1233,
+      status: "active",
+      price: "$19000.00",
+    },
+    {
+      id: 5,
+      name: "Archer fish",
+      img:
+        "https://th.bing.com/th/id/OIP.9PfEyJ9Ru0xV_Iw6Dg0rTQHaFS?w=260&h=186&c=7&r=0&o=5&dpr=1.38&pid=1.7",
+      stock: 18723,
+      status: "active",
+      price: "$100.00",
+    },
+    {
+      id: 6,
+      name: "Bala shark",
+      img:
+        "https://th.bing.com/th/id/OIP.LJeBVF4h5FsICAD7v1X7qAHaGb?w=230&h=199&c=7&r=0&o=5&dpr=1.38&pid=1.7",
+      stock: 1233,
+      status: "active",
+      price: "$120.00",
+    },
+    {
+      id: 7,
+      name: "Betta fish",
+      img:
+        "https://th.bing.com/th/id/OIP.Gd8ETGwZsjARKoJb662VtwHaFj?w=229&h=180&c=7&r=0&o=5&dpr=1.38&pid=1.7",
+      stock: 1023,
+      status: "active",
+      price: "$150.00",
+    },
+    {
+      id: 8,
+      name: "Blood parrot",
+      img:
+        "https://th.bing.com/th/id/R.98829c94728ba0270365254808938bf0?rik=J9MaiYFLL9xcNQ&riu=http%3a%2f%2fstatic1.squarespace.com%2fstatic%2f56cc4852356fb0ad1dc8bc20%2ft%2f5793a240be65943e16c4338e%2f1469293166824%2fScreen%2bShot%2b2016-07-23%2bat%2b17.27.36.png&ehk=uyxoibiJAWuUdQW5FVZH1OURJmj7z3awLS6QEhPQnhA%3d&risl=&pid=ImgRaw&r=0",
+      stock: 19923,
+      status: "active",
+      price: "$120.00",
+    },
+    {
+      id: 9,
+      name: "Blue dolphin moorii",
+      img:
+        "https://th.bing.com/th/id/OIP.Oyy1i2wLudqTfJEksOZ3jQHaE8?w=264&h=180&c=7&r=0&o=5&dpr=1.38&pid=1.7",
+      stock: 12443,
+      status: "active",
+      price: "$1203.00",
+    },
+    {
+      id: 10,
+      name: "Buenos Aires tetra",
+      img:
+        "https://th.bing.com/th/id/OIP.diY6H9V_p0G1DN-xKbUAlwHaGe?w=222&h=194&c=7&r=0&o=5&dpr=1.38&pid=1.7",
+      stock: 1233,
+      status: "active",
+      price: "$1220.00",
+    },   
+  ];
+  export const orderRows = [
+    {
+      id: 101,
+      userId: 1,
+      orderDate: "23rd Jan,2021",
+      address: "West Virginia, United States",
+      status: "Delivered",
+    },
+    {
+      id: 102,
+      userId: 2,
+      orderDate: "25 March 2021",
+      address: "lorida, United States",
+      status: "Delivered",
+    },
+    {
+      id: 103,
+      userId: 3,
+      orderDate: "7 April 2020",
+      address: "Missouri, United States",
+      status: "Delivered",
+    },
+    {
+      id: 104,
+      userId: 4,
+      orderDate: "23rd Jan,2021",
+      address: "New Mexico, United States",
+      status: "Delivered",
+    },
+    {
+      id: 105,
+      userId: 5,
+      orderDate: "20 October 2020",
+      address: "Oregon, United States",
+      status: "Cancelled",
+    },
+    {
+      id: 106,
+      userId: 6,
+      orderDate: "15 April 2021",
+      address: "Indiana, United States",
+      status: "Delivered",
+    },
+    {
+      id: 107,
+      userId: 7,
+      orderDate: "23rd Jan,2021",
+      address: "South Dakota, United States",
+      status: "Delivered",
+    },
+    {
+      id: 108,
+      userId: 8,
+      orderDate: "3 November 2021",
+      address: "Minnesota, United States",
+      status: "Delivered",
+    },
+    {
+      id: 109,
+      userId: 9,
+      orderDate: "23rd Jan,2021",
+      address: "Illinois, United States",
+      status: "Cancelled",
+    },
+    {
+      id: 110,
+      userId: 10,
+      orderDate: "27 November 2021",
+      address: "Delaware, United States",
+      status: "Delivered",
+    },
+  ];
