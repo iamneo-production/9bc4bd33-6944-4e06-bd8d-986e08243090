5207f8414cdb32313e0b97e4f3674c2cdb70f0a6
Shreya Singh <72337463+Shreya479@users.noreply.github.com>
2022-03-14 19:36:54 +0530

    Update App.js

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,22 +1,70 @@
+import Sidebar from "./components/sidebar/Sidebar";
+import Topbar from "./components/topbar/Topbar";
 import "./App.css";
-import styled from "styled-components";
-import { AccountBox } from "./components/accountBox"; 
-
-const AppContainer = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`;
-
+import Login from './pages/login/Login';
+import ForgetPassword from './pages/forgetPassword/ForgetPassword';
+import FirstPage from './pages/firstPage/FirstPage';
+import Home from "./pages/home/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
+import NewUser from "./pages/newUser/NewUser";
+import ProductList from "./pages/productList/ProductList";
+import Product from "./pages/product/Product";
+import NewProduct from "./pages/newProduct/NewProduct";
+import OrderList from "./pages/orderList/OrderList";
 function App() {
   return (
-    <AppContainer>
-      <AccountBox />
-    </AppContainer>
+    <Router>
+      
+      <div className="container">
+        <Switch>
+        <Route exact path="/">
+            <FirstPage />
+          </Route>
+          <Route path="/Login">
+            <Login />
+          </Route>
+          <Route path="/ForgetPassword">
+            <ForgetPassword />
+          </Route>
+          <Route path="/Home">
+          
+            <Sidebar/>
+           
+            <Home />
+          </Route>
+          <Route path="/users">
+          <Sidebar/>
+            <UserList />
+          </Route>
+          <Route path="/user/:userId">
+          <Sidebar/>
+            <User />
+          </Route>
+          <Route path="/newUser">
+          <Sidebar/>
+            <NewUser />
+          </Route>
+          <Route path="/products">
+          <Sidebar/>
+            <ProductList />
+          </Route>
+          <Route path="/product/:productId">
+          <Sidebar/>
+            <Product />
+          </Route>
+          <Route path="/newproduct">
+            <Sidebar/>
+            <NewProduct />
+          </Route>
+          <Route path="/orders">
+          <Sidebar/>
+            <OrderList />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
 export default App;
