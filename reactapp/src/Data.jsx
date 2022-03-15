68a8061a2dec75ab971a0ecbf7b5a721cfd79eac
Shreya Singh <72337463+Shreya479@users.noreply.github.com>
2022-03-15 22:29:51 +0530

    Add files via upload

---
+++ reactapp/src/Data.jsx
@@ -0,0 +1,60 @@
+const Data = [
+    {
+        id: 0,
+        title: 'Adolfo’s Catfish',
+        price: 530,
+        desc: 'Size	up to 2.2 inches (5.7 centimeters). The fish isn’t troublesome in terms of nutrition, and it eats all kinds of food. Adolfo’s catfish should have a diversified diet, and you should feed it with different food types. Frozen, live, artificial – the fish eats all kinds of food. Catfish also likes special pellets for catfish. The main issue is that there isn’t much food that gets to the tank bottom, because most of it is eaten by the fishes in the middle water layers. If you see that your catfish don’t have enough food, feed them after you turn off the light.',
+        img: '/assets/image/product1.jpg'
+    },
+    {
+        id: 1,
+        title: 'Bala Shark',
+        price: 99,
+        desc: 'Size	up to 25-30 cm (10-12 in). The fish body is thin and slender, flattened from sides. The head is small, the eyes are very large, and the fish has an inferior mouth with no barbs.The scales are very large, and the fins are sickle-shaped. The dorsal is tall and vertical, and its fluke is bifurcated. The body is silver steel-colored, large scales create sparkles. In pet shops, as a rule, you see the fish about 5 cm long (2 in). But don’t be fooled with this small size because these are just juveniles.',
+        img: '/assets/image/product2.jpg'
+    },
+    {
+        id: 2,
+        title: 'Badis Badis',
+        price: 640,
+        desc: 'Size	3 in (7-8 cm). The body coloring is quite unstable and changeable. It depends on tank conditions, the physiological state of the fish, and its mood. It is especially true for males. In several seconds their coloring may change unrecognizably. Exited males coloring when they have fights between them or during the spawning period look especially good. The female fish body has significantly simpler coloring with brown colors prevailing.',
+        img: '/assets/image/product3.jpg'
+    },
+    {
+        id: 3,
+        title: 'Archer Fish',
+        price: 530,
+        desc: 'The adult fish species grow to be about 30 cm (12 in) long. In a tank fish size is a bit smaller 12–18 cm (5–7 in). Their lifespan is up to 10 years. Fish of this kind have body flattened from sides and a bit protrusive mandible. The body has bright silver coloring with 5-6 vertical stripes/spots. Due to its large eyes the fish can find insects that are above water, for example, when they are sitting on a tree leave not far from the water surface.',
+        img: '/assets/image/product4.jpg'
+    },
+    {
+        id: 4,
+        title: 'Red-Tailed Shark',
+        price: 240,
+        desc: 'The red-tailed shark is an ornamental fish for an aquarium that makes for an impressive addition. These species are deep black fish with a dazzling bright red tail and are native to Thailand. The red-tailed sharks require a tank of 55 gallons with rocks, caves, tropical plants along with a well-maintained temperature and water conditions. Because of the elaborate setup, this fish is not suitable for beginners. This fish eats a variety of fish flakes and live prey.',
+        img: '/assets/image/product8.jpg'
+    },
+    {
+        id: 5,
+        title: 'Guppies',
+        price: 135,
+        desc: 'The guppies are perhaps the most popular freshwater aquarium fish called million fish or rainbow fish. These fishes can survive for over a week without being fed, making them the hardiest aquarium fish. Guppies are happy with brine shrimp, blood worms, Daphnia or even regular fish flakes because they are not very selective in the diet. You can easily choose the one that suits your style because these lovely aquarium fish come in various beautiful colour combinations. However, remember that guppies love to reproduce. Therefore, it is best to keep male and female guppies separate unless you intend to rear them for breeding purposes.',
+        img: '/assets/image/product6.jpg'
+    },
+    {
+        id: 6,
+        title: 'Mickey Mouse Platey',
+        price: 590,
+        desc: 'Platy is a freshwater fish that can be a good choice in an aquarium, originating from Southern Mexico and the east coast of Central America. The Mickey mouse platy are non-aggressive fishes and fit well with other species because of their friendly nature. The platies will eat most of the different flakes or even frozen live foods because they are not very demanding for feeding. These fishes are available in various colours and sizes because of selective breeding over the years. However, the creamy golden-white body and the black tail is the prominent feature of the Mickey mouse platey, and it is very suitable for beginners.',
+        img: '/assets/image/product5.jpg'
+    },
+    {
+        id: 7,
+        title: 'Freshwater Angelfish',
+        price: 500,
+        desc: 'Size	6 inches (15 cm) and about 10 in (25 cm) tall. In the wild, angelfish have a silvery body flattened from sides with dark stripes, large fins, and sharpened head shape. Reproductive fish fluke may have some long thin rays. Such fin shape helps the fish to hide among plant roots and leaves. For the same reason, in the wild, it has vertical dark stripes on its body. However, it’s a rather difficult task to describe angelfish that are on sale nowadays. As a result of line breeding made by amateurs, a huge number of angelfish color variations were obtained – from golden to black',
+        img: '/assets/image/product7.jpg'
+    },
+];
+
+export default Data
