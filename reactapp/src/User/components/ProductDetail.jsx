51eff4fcf000f1edec76edff9c68b47717c26f78
samiran2076 <77327194+samiran2076@users.noreply.github.com>
2022-03-26 22:42:44 +0530

    review routed

--- reactapp/src/User/components/ProductDetail.jsx
+++ reactapp/src/User/components/ProductDetail.jsx
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom'
 import Data from '../Data'
 import { useDispatch } from 'react-redux'
 import { addItem, delItem } from '../redux/action/index'
+import { Link } from "react-router-dom";
+
 
 const ProductDetail = () => {
     const [cartBtn, setCartBtn] = useState("Add to Cart")
@@ -28,15 +30,18 @@ const ProductDetail = () => {
             <div className="container my-5">
                 <div className="row">
                     <div className="col-md-6 d-flex justify-content-center mx-auto product">
-                        <img src={product.img} alt="" height={400} />
+                        <img src={product.img} alt="" height={450} width={600} />
                     </div>
                     <div className="col-md-6 d-flex justify-content-center flex-column">
                         <h1 className='display-5 fw-bold'>{product.title}</h1>
                         <hr />
                         <h2 className='my-3'>$ {product.price}</h2>
+                        
+                        <Link to="reviews" style={{paddingTop:"10px",paddingBottom:"10px"}} className="review">Reviews</Link>
                         <p>{product.desc}</p>
-                        <button onClick={() => handleCart(product)} className='btn btn-outline-primary'>{cartBtn}</button>
-                        <button className='btn btn-outline-primary m-4 rounded btn-block'>Add to WishList</button>
+                        <div class="btn-group" role="group">
+                        <button onClick={() => handleCart(product)} className='btn btn-outline-primary m-4 rounded btn-block'>{cartBtn}</button>
+                        </div>
                     </div>
                 </div>
             </div>
