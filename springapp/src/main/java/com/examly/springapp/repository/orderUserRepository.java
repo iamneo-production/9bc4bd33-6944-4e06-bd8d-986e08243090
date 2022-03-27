023087ff788a2612279393a1f54cb343b0babcc7
kaushick <82633569+kaushick2000@users.noreply.github.com>
2022-03-27 15:05:35 +0530

    Add files via upload

---
+++ springapp/src/main/java/com/examly/springapp/repository/orderUserRepository.java
@@ -0,0 +1,11 @@
+package com.examly.springapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import com.examly.springapp.model.OrderModel;
+
+@Repository
+public interface orderUserRepository extends JpaRepository<OrderModel, Long> {
+    
+}
