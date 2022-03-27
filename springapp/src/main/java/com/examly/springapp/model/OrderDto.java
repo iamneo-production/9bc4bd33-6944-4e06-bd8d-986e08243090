47a30fce15ac73eb71dd4edec3cd10ab8ca003fd
kaushick <82633569+kaushick2000@users.noreply.github.com>
2022-03-27 15:32:58 +0530

    Update OrderDto.java

--- springapp/src/main/java/com/examly/springapp/model/OrderDto.java
+++ springapp/src/main/java/com/examly/springapp/model/OrderDto.java
@@ -3,14 +3,17 @@ import org.springframework.stereotype.Component;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
-
+import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderDto {
-    private Long id;
+    private int id;
     private String name;
-    private String date;
+     @JsonFormat(pattern="dd-MM-yyyy",shape=Shape.STRING)
+    private Date date = new Date();
     private String status;
 }
