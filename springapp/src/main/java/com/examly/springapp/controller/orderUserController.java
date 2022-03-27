7c8281117ee22a2ad05ff935bd2753a835888a12
kaushick <82633569+kaushick2000@users.noreply.github.com>
2022-03-27 15:33:47 +0530

    Update orderUserController.java

--- springapp/src/main/java/com/examly/springapp/controller/orderUserController.java
+++ springapp/src/main/java/com/examly/springapp/controller/orderUserController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.PathVariable;
+
 import java.util.Map;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.http.ResponseEntity;
@@ -43,7 +44,7 @@ public class orderUserController {
     }
 
     @GetMapping("/Order/{id}")
-    public OrderModel findOrderID(@PathVariable Long id){
+    public OrderModel findOrderID(@PathVariable int id){
         return serviceModel.getOrderById(id);
     }
     @PostMapping("/addOrder")
