55a4df84de8d729351f05bdceff7bba5caa2cc09
Shreya Singh <72337463+Shreya479@users.noreply.github.com>
2022-03-30 13:55:23 +0530

    Update Sidebar.jsx

--- reactapp/src/Admin/components/sidebar/Sidebar.jsx
+++ reactapp/src/Admin/components/sidebar/Sidebar.jsx
@@ -1,28 +1,39 @@
-import "./sidebar.css";
+import React from 'react'
+import './sidebar.css'
 import { Link } from "react-router-dom";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { useHistory } from 'react-router-dom';
-const SideBar = ()=>{
-  const history = useHistory();
+const SideBar = ()=>{  
     return(
     <>   
     <div className="sidebar">
     <div className="top-left">
     <h1 className="logo">Fish Landia</h1>
-    </div> 
-      <div className="sidebarWrapper">
-      <div className="sidebarMenu">
-          <ul className="sidebarList">
-          <button className='button' onClick={() => history.push('/Home')} > HOME </button><br></br>
-          <button className='button' onClick={() => history.push('/users')} > USERS </button><br></br>
-          <button className='button' onClick={() => history.push('/Products')} > PRODUCTS </button><br></br>
-          <button className='button' onClick={() => history.push('/orders')} > ORDERS </button><br></br>
-          </ul>
-          <button className='button' onClick={() => history.push('/')} > LOGOUT </button>
-        </div>
-      </div>
-    </div>
-    </>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/Home">HOME</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/users">USERS</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/products">PRODUCTS</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/orders">ORDERS</Link>
+                            </li>
+                        </ul>
+                        <Link className="navbar-brand mx-auto fw-bold" to="/">LOGOUT</Link>
+                    </div>
+                </div>
+            </nav>
+            </div>
+            </div>
+        </>
   );
 }
 export default SideBar;
