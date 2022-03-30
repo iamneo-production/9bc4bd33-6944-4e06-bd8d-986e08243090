adeebc26b72f4f2d2c173cdc488171e54bfb1d51
Shreya Singh <72337463+Shreya479@users.noreply.github.com>
2022-03-30 13:41:33 +0530

    Update App.js

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,95 +1,50 @@
-import Sidebar from "./Admin/components/sidebar/Sidebar";
-import Topbar from "./Admin/components/topbar/Topbar";
-import "./App.css";
-import Login from './Admin/pages/login/Login';
-import ForgetPassword from './Admin/pages/forgetPassword/ForgetPassword';
-import FirstPage from './Admin/pages/firstPage/FirstPage';
-import Home from "./Admin/pages/home/Home";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import UserList from "./Admin/pages/userList/UserList";
-import User from "./Admin/pages/user/User";
-import NewUser from "./Admin/pages/newUser/NewUser";
-import ProductList from "./Admin/pages/productList/ProductList";
-import Product from "./Admin/pages/product/Product";
-import NewProduct from "./Admin/pages/newProduct/NewProduct";
-import OrderList from "./Admin/pages/orderList/OrderList";
+import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import Home from './User/components/Home'
+import UserHome from './User/components/UserHome';
 import About from './User/components/About'
-import Product from './User/components/Product'
+import UserProduct from './User/components/UserProduct';
 import Header from './User/components/Header'
+import ProductDetail from './User/components/ProductDetail';
 import Helpdesk from './User/components/Helpdesk'
 import Review from './User/components/Review';
-import Orders from './User/components/Orders';
-import Tracking from './User/components/Tracking';
-import ProductDetail from './User/components/ProductDetail';
+import FirstPage from './firstPage/FirstPage';
+import Sidebar from './Admin/components/sidebar/Sidebar';
+import Topbar from './Admin/components/topbar/Topbar';
+import Login from './Admin/pages/login/Login';
+import ForgetPassword from './Admin/pages/forgetPassword/ForgetPassword';
+import Home from './Admin/pages/home/Home';
+import UserList from './Admin/pages/userList/UserList';
+import User from './Admin/pages/user/User';
+import NewUser from './Admin/pages/newUser/NewUser';
+import ProductList from './Admin/pages/productList/ProductList';
+import Product from './Admin/pages/product/Product';
+import NewProduct from './Admin/pages/newProduct/NewProduct';
+import OrderList from './Admin/pages/orderList/OrderList';
+import { Fragment } from 'react';
 function App() {
   return (
-    <Router>
-      <div className="container">
-        <Switch>
-        <Route exact path="/">
-            <FirstPage />
-          </Route>
-          <Route path="/Login">
-            <Login />
-          </Route>
-          <Route path="/ForgetPassword">
-            <ForgetPassword />
-          </Route>
-          <Route path="/Home">
-            <Sidebar/>
-            <Home />
-          </Route>
-          <Route path="/users">
-          <Sidebar/>
-            <UserList />
-          </Route>
-          <Route path="/user/:userId">
-          <Sidebar/>
-            <User />
-          </Route>
-          <Route path="/newUser">
-          <Sidebar/>
-            <NewUser />
-          </Route>
-          <Route path="/products">
-          <Sidebar/>
-            <ProductList />
-          </Route>
-          <Route path="/product/:productId">
-          <Sidebar/>
-            <Product />
-          </Route>
-          <Route path="/newproduct">
-            <Sidebar/>
-            <NewProduct />
-          </Route>
-          <Route path="/orders">
-          <Sidebar/>
-            <OrderList />
-          </Route>
-    <Route path="/orders">
-          <Sidebar/>
-            <Orders />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
     <>
-      <Header />
+    <div>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/product' element={<Product />} />
-        <Route path='/product/:id' element={<ProductDetail />} />\
-          <Route path='/product/:id/reviews' element={<Review />}/>
-        <Route path='/helpdesk' element={<Helpdesk />} />
-          <Route path='/orders' element={<Orders/>}/>
-         <Route path="/view-order/:id" element={<Tracking/>}/>
-           <Route path="/customer-service" element={<Helpdesk />}/>
-//             <Route path="/bill-details" element={Orders}/>
+        <Route exact path='/' element={<FirstPage />} />
+        <Route path='/userHome' element={<Fragment><Header /><UserHome /></Fragment>} />
+        <Route path='/about' element={<Fragment><Header /><About /></Fragment>} />
+        <Route path='/product' element={<Fragment><Header /><UserProduct /></Fragment>} />
+        <Route path='/product/:id' element={<Fragment><Header /><ProductDetail /></Fragment>} />
+        <Route path='/product/:id/reviews' element={<Fragment><Header /><Review /></Fragment>}/>
+        <Route path='/helpdesk' element={<Fragment><Header /><Helpdesk /></Fragment>} />
+        <Route path='/admin/login' element={<Login />} />
+        <Route path='/ForgetPassword' element={<ForgetPassword />} />
+        <Route path='/Home' element={<Fragment><Sidebar /><Home /></Fragment>} />
+        <Route path='/users' element={<Fragment><Sidebar /><UserList /></Fragment>} />
+        <Route path='/user/:userId' element={<Fragment><Sidebar/><User /></Fragment>} />
+        <Route path='/newUser' element={<Fragment> <Sidebar/><NewUser /></Fragment>} />
+        <Route path='/products' element={<Fragment><Sidebar/><ProductList /></Fragment>} />
+        <Route path='/products/:productId' element={<Fragment><Sidebar/><Product /></Fragment>} />
+        <Route path='/newproduct' element={<Fragment><Sidebar/> <NewProduct /></Fragment>} />
+        <Route path='/orders' element={<Fragment><Sidebar/> <OrderList /></Fragment>} />       
       </Routes>
+      </div>    
     </>
   );
 }
