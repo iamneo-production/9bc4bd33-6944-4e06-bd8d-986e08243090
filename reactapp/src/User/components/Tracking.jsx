6e82873757d4c908b6d349d2c0e0d7ef671fffaa
kaushick <82633569+kaushick2000@users.noreply.github.com>
2022-04-05 13:38:22 +0530

    Update Tracking.jsx

--- reactapp/src/User/components/Tracking.jsx
+++ reactapp/src/User/components/Tracking.jsx
@@ -1,111 +1,79 @@
-import React,{ Component } from 'react'
-import './Tracking.css'
+import React from "react";
+import "react-step-progress-bar/styles.css";
+import { ProgressBar, Step } from "react-step-progress-bar";
+ 
 import {Link} from 'react-router-dom';
 
-
 class Tracking extends React.Component {
-  constructor(props)
-  {
-      super(props)
-      this.state={
-          id:this.props.match.params.id,
-          order :{}
-      }
-  }
-
-  customerService()
-  {
-      this.props.history.push(`/customer-service`);
-  }
-  billDetails()
-  {
-      this.props.history.push(`/bill-details`);
-  }
-  
-  
-
   render() {
-    
     return (
-      
-      <div className="main_container">
-     <div class="container padding-bottom-3x mb-1">
-        <div class="card mb-3">
-         <div class="d-flex flex-wrap flex-sm-nowrap justify-content-between py-3 px-2 bg-secondary">
-           
-//             <div>Order Status : { this.state.order.status }</div> 
-            <div>Order Status: {this.props.location.state.status}</div>
-          </div>
-          
-          <div class="card-body">
-            <div class="steps d-flex flex-wrap flex-sm-nowrap justify-content-between padding-top-2x padding-bottom-1x">
-              
-              <div class="step completed">
-                <div class="step-icon-wrap">
-                  <div class="step-icon"><i class="pe-7s-cart"></i></div>
+        <div>
+            <div className="container py-5">
+                <div className="row">
+                    <div className="col-12 text-center">
+                        <h1>TRACKING</h1>
+                        <hr />
+                    </div>
                 </div>
-                <h4 class="step-title">Ordered</h4>
-              </div>
-            
-              <div class="step completed">
-                <div class="step-icon-wrap">
-                  <div class="step-icon"><i class="pe-7s-config"></i></div>
-                </div>
-                <h4 class="step-title">Dispached</h4>
-              </div>
-              <div class="step ">
-                <div class="step-icon-wrap">
-                  <div class="step-icon"><i class="pe-7s-medal"></i></div>
-                </div>
-                <h4 class="step-title">Out for Delivery</h4>
-              </div>
-              <div class="step ">
-              <div class="step-icon-wrap">
-                  <div class="step-icon"><i class="pe-7s-home"></i></div>
-                </div>
-                <h4 class="step-title">Delivered</h4>
-              </div> 
-              
             </div>
-          </div>
-        </div>
-        
-          <Link to="\bill-details">
-          <div  onClick={() => this.billDetails()} class="text-left text-sm-right"><button class="bill" style={{backgroundColor: "#0da9ef", color: "#fff",borderRadius:"2%"}}>View Bill Details</button></div>
-          </Link>
-        </div>
-        
-        <div class="card card1 ">
-          <div className="card-body">
-            
-          
-            {/* <div><button style={{width:"60%",marginLeft:"8%",borderRadius:"6px",backgroundColor: "#0da9ef", color: "#fff",marginBottom:"2%"}} >Cancel</button></div>
-            */}
-            <div class="customer">
-            {/* <li> */}
-            {/* <Link to="/customer-service"> */}
-            
-            <button onClick={() => this.customerService()} style={{width:"60%",marginLeft:"8%",borderRadius:"6px",backgroundColor: "#0da9ef", color: "#fff"}}>Contact Customer Service</button>
 
-            {/* </Link> */}
-            {/* </li> */}
-            </div>
+
+        <div  style={{width:"70%",marginLeft:"18%",pointerEvents:"none"}}>
+         
+      <ProgressBar 
+        percent={40}
+        filledBackground="linear-gradient(to right, #0da9ef, #0da9ef)"
+      >
+        <Step transition="scale">
+          {({ accomplished }) => (
+            <img
+              style={{ filter: `grayscale(${accomplished ? 0 : 80}%)`,marginBottom:"95%",marginLeft:"50%"}}
+              width="50"
+              src="https://cdn-icons.flaticon.com/png/128/2543/premium/2543369.png?token=exp=1649089283~hmac=7f2904e260b65336988157a615cda407"
+            />
             
-             
-            
-           
-            </div>
-  
-        </div>
+          ) 
+          }
+        </Step>
         
+        <Step transition="scale">
+          {({ accomplished }) => (
+            <img
+              style={{ filter: `grayscale(${accomplished ? 0 : 80}%)`,marginBottom:"80%" }}
+              width="50"
+              src="https://cdn-icons.flaticon.com/png/128/2769/premium/2769339.png?token=exp=1649089325~hmac=46b4dd42066c4240e81ce7e78b9ad241"       
+              />
+          )}
+        </Step>
+        <Step transition="scale">
+          {({ accomplished }) => (
+            <img
+              style={{ filter: `grayscale(${accomplished ? 0 : 80}%)`,marginBottom:"90%" }}
+              width="50"
+              src="https://cdn-icons.flaticon.com/png/128/5637/premium/5637217.png?token=exp=1649086830~hmac=b5ae4e8fe94cc9430f3e1b30d170a9a6"
+            />
+          )}
+        </Step>
+        <Step transition="scale">
+          {({ accomplished }) => (
+            <img
+              style={{ filter: `grayscale(${accomplished ? 0 : 80}%)` ,marginBottom:"100%",marginRight:"100%"}}
+              width="50"
+              src="https://cdn-icons.flaticon.com/png/128/384/premium/384082.png?token=exp=1649086926~hmac=96fe240d745646902bf53317eedeae4f"
+            />
+          )}
+        </Step>
+        
+      </ProgressBar>
+      <span >Ordered</span>
+      <span style={{marginLeft:"25%"}}>Dispatched</span>
+      <span style={{marginLeft:"25%"}}>Out For Delivery</span>
+      <span style={{marginLeft:"16%"}}>Delivered</span>
       </div>
       
-      
+      <button className="btn btn-primary" style={{marginLeft:"40%" ,marginTop:"10%"}} ><Link to={{ pathname: `/customer-service`  }} style ={{ color:"white"}} >Contact Customer Service</Link></button>
+      </div>
     );
   }
-  
 }
-
- export default Tracking;
-
-
+export default Tracking;
